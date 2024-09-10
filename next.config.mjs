7dd61c5f08c78e9f47a79186b3eae23e20efd7a4
Sickenings <109198682+Sickenings@users.noreply.github.com>
2024-09-09 23:36:14 -0400

    idk

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,13 @@ const nextConfig = {
  
   images: {
     domains: ['i.ibb.co'], // Agrega el dominio aquí
+    reactStrictMode: true,
+  output: 'export', // This enables static export
+  images: {
+    unoptimized: true, // Disable image optimization if needed
   },
+},
+
 }; // Your other configuration options
 
 
